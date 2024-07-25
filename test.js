a10b9ecf0575d753547e5e2c78e8b0226fa3c766
mynameee <alldebaro@gmail.com>
2024-07-25 16:04:03 +0900

    데이터로드

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
-wefwef
-dddaaa
+function fetchData(callback) {
+    setTimeout(() => {
+        callback("데이터 로드 완료");
+    }, 2000);
+}
+
+fetchData((message) => {
+    console.log(message); // "데이터 로드 완료" (2초 후 출력)
+});
+
+console.log("finished!")
