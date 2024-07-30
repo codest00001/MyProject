876a7605df61652e0607ba205eb40c83815a10c3
mynameee <alldebaro@gmail.com>
2024-07-30 17:14:49 +0900

    Initial commit

---
+++ test1.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')({sigint:true});
+
+let targetNumber = Math.floor(Math.random() * 100) + 1;
+let guess = null;
+
+while (guess !== targetNumber) {
+    guess = parseInt(prompt("Guess a number between 1 and 100:"));
+
+
+    // 만약에 타겟넘버보다 입력값이 크면 "그것보다 작아요"라고 출력해주자.
+    // 만약에 타겟넘버보다 입력값이 작으면 "그것보다 커요!"라고 출력해주자.
+    // 만약에 타겟넘버와 입력값이 같으면 정답이에요!라고 출력하고 while을 종료.
+    
+
+    if (targetNumber > guess){
+        console.log("그것보다 커요!")
+    } else if (targetNumber < guess){
+        console.log("그것보다 작아요!")
+    } else {
+        console.log("정답이에요!")
+        break;
+    }
+   } 
+
+   if ({}) {
+    console.log("성인입니다.");
+}
